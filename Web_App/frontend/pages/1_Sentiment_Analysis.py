338f788ac5003e4aa9df3f8caa3c25dbaa43e5ff
Pan LIU <liupan.lucine@gmail.com>
2023-03-04 18:54:16 +0100

    correct a speelling error and change a variable name

--- Web_App/frontend/pages/1_Sentiment_Analysis.py
+++ Web_App/frontend/pages/1_Sentiment_Analysis.py
@@ -2,20 +2,20 @@ import streamlit as st
 import json
 import requests
 
-class_names = ["joy","sadness","superise","disgust","anger", "fear", "trust", "anticipation"]
+class_names = ["joy","sadness","surprise","disgust","anger", "fear", "trust", "anticipation"]
 
 st.title("💯Sentiment analysis for movie reviews based on Bert")
-review_text = st.text_input("Please enter a movie review that you want to analyze: ")
+input_text = st.text_input("Please enter a movie review that you want to analyze: ")
 epoch = st.selectbox("Choose the epoch ", [i for i in range(1, 11)])
 if st.button("Submit"):
-    if review_text:
+    if input_text:
         # files = {"file": ("review.txt", review_text)}
-        data = {"review": review_text}
+        data = {"review": input_text}
         try:
             response = requests.post(f"http://0.0.0.0:8080/sentiment_classification/{epoch}", json=data)
             response.raise_for_status()
             prediction = response.json()
-            st.write(f'Review text: {review_text}')
+            st.write(f'Review text: {input_text}')
             st.write(f'Sentiment  : {class_names[prediction.get("output")]}')
         except requests.exceptions.HTTPError as err:
             st.error(f"HTTP error occurred: {err}")
